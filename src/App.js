0fa649ff709e58fdde519cd696a3c8c635d43e83
patrykomiotek <patryk.omiotek@gmail.com>
2020-08-23 11:08:04 +0200

    first event handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+function clickHandler(event) {
+  // event.preventDefault();
+  console.log('Hello from click handler!');
+}
+
+function Button(props) { // { color, text }
+  return <button onClick={clickHandler}>Click me, {props.text}</button>
+}
+Button.defaultProps = {
+  text: 'Hello!!!'
+}
+
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Button color="blue" text="Hello WarsawJS" />
     </div>
   );
 }
