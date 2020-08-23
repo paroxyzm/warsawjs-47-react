c4f98b663369cdf5223b0f8c8ae018b8702d7de5
patrykomiotek <patryk.omiotek@gmail.com>
2020-08-23 13:10:15 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Link // NavLink
+} from 'react-router-dom';
 
 // import UsersContainer from '@abc/users/UsersContainer';
 import UsersContainer from './users/containers/UsersContainer';
@@ -19,7 +25,27 @@ function App() {
   return (
     <div className="app">
       <Button color="blue" text="Hello WarsawJS" />
-      <UsersContainer />
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/money">Currencies</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/users">
+            <UsersContainer />
+          </Route>
+          <Route path="/money">
+
+          </Route>
+          <Route path="/">
+            
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
