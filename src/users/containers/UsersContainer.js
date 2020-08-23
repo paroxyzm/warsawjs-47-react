a168dd0336ebc362abfdd54665a7cf742eb8229f
patrykomiotek <patryk.omiotek@gmail.com>
2020-08-23 12:16:19 +0200

    fetchiung data using hooks

--- src/users/containers/UsersContainer.js
+++ src/users/containers/UsersContainer.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 
 import UsersList from '../components/UsersList'
 
-class UsersContainer extends Component {
+function UsersContainer() {
 
-  state = {
-    users: [] // [{id, name, username}, {id, name, username}]
-  }
+  // const result = useState([]);
+  // result[0] // data: []
+  // result[1] // fn, for modify result[0]
+  // changeData = result[1]
+  // changeData([1, 2, 3, 4])
+  const [ users, setUsers ] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(data => this.setState({ users: data }));
-  }
+    .then(data => setUsers(data));
+  }, []);
 
-  render() {
-    const { users } = this.state;
-    return <UsersList users={users} />
-  }
+  return <UsersList users={users} />
 }
+
+// class UsersContainer extends Component {
+
+//   state = {
+//     users: [] // [{id, name, username}, {id, name, username}]
+//   }
+
+//   componentDidMount() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(data => this.setState({ users: data }));
+//   }
+
+//   render() {
+//     const { users } = this.state;
+//     return <UsersList users={users} />
+//   }
+// }
 
 export default UsersContainer;
